177d5584bc5fbddefe8adb7d48b684cc0c0eae79
PallabKumarS <pallabkumar26@gmail.com>
2025-01-07 14:55:09 +0600

    started using rtk query

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./features/counter/counterSlice";
 import taskSlice from "./features/tasks/taskSlice";
 import userSlice from "./features/users/userSlice";
 // import logger from "./middlewares/logger";
 
 export const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
     taskStore: taskSlice.reducer,
     userStore: userSlice.reducer,
   },
